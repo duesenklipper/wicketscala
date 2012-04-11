ad069fa18471fd69bd9bf9d1db7915b92e03df76
Carl-Eric Menzel <cmenzel@wicketbuch.de>
2012-04-11 14:53:52 +0200

    ScalaWicketstuffPage done

--- src/main/scala/de/wicketbuch/wicketscala2/ScalaWicketstuffPage.scala
+++ src/main/scala/de/wicketbuch/wicketscala2/ScalaWicketstuffPage.scala
@@ -4,8 +4,15 @@ package de.wicketbuch.wicketscala2 {
   import org.apache.wicket.model.IModel
   import org.wicketstuff.scala.Fodel
   import org.wicketstuff.scala.ScalaWicket
+  import org.apache.wicket.markup.html.form.NumberTextField
+  import org.apache.wicket.behavior.Behavior
+  import org.apache.wicket.Component
+  import org.apache.wicket.markup.ComponentTag
+  import org.apache.wicket.markup.html.basic.Label
+  import org.wicketstuff.scala.SLabel
+  import org.wicketstuff.scala.SLink
 
-  class Person(var alter: Int)
+  class Person(var alter: Integer)
 
   class ScalaWicketstuffPage extends WebPage with ScalaWicket {
 
@@ -14,10 +21,23 @@ package de.wicketbuch.wicketscala2 {
 
     val person = new Person(17)
 
-    val alterModel: IModel[Int] = new Fodel(person.alter, person.alter = _)
+    val alterModel: IModel[Integer] = new Fodel(person.alter, person.alter = _)
 
-    // alternativ:
-    // implizit konvertierte funktion, nur get, nicht set:
-    var numberModelImplicit: IModel[Int] = { number }
+    val alterField = new NumberTextField[Integer]("alterField", alterModel, classOf[Integer])
+
+    alterForm.add(alterField)
+
+    alterField.add(new Behavior() {
+      override def onComponentTag(c: Component, tag: ComponentTag) {
+        tag.put("style", if (alterModel.getObject() >= 18) "background-color:#88ee88" else "background-color:#ee8888")
+      }
+    })
+
+    add(new Label("showAlter", alterModel))
+
+    add(new SLabel("showVolljaehrig", if (alterModel.getObject() >= 18) "Volljährig" else "Minderjährig"))
+
+    add(new SLink("incrementor", { alterModel.setObject(alterModel.getObject() + 1) }))
   }
+
 }
